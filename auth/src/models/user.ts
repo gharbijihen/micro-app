6bfe891e5c3be4d215a4608aca52f57b0e8d448c
jihen <gharbi.jihenn@gmail.com>
2025-06-24 14:57:39 +0100

    v1

---
+++ auth/src/models/user.ts
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+import { Password } from "../services/password";
+import { UserDoc } from "../types/IUser";
+import jwt from "jsonwebtoken";
+
+// An interface that describes the properties
+// that are requried to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
+});
+
+//return JWT token
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this.id, email: this.email }, process.env.JWT_KEY!, {
+    expiresIn: 3600,
+  });
+};
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
